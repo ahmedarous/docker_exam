e40407c967430840def7974198c6dcf912ee6bda
Ahmed AROUS <arous.ahmed86@gmail.com>
2022-01-21 12:37:12 +0100

    files

---
+++ python/run.py
@@ -0,0 +1,16 @@
+from flask import (Flask, render_template, send_from_directory)
+app = Flask(__name__, static_folder='/myvol')
+
+
+@app.route("/index")
+def start():
+    return send_from_directory('/myvol/', "index.html")
+
+
+@app.route("/hello")
+def welcome():
+    return render_template("welcome.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
